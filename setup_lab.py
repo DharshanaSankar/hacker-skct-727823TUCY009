947ae5b7815a1bf83c4093f21d901a8031cc143c
Dharshana Sankar <149609667+DharshanaSankar@users.noreply.github.com>
2026-03-29 15:10:55 -0400

    Delete setup_lab.py

--- setup_lab.py
+++
@@ -1,9 +0,0 @@
-# Dharshana - 727823TUCY009
-
-import datetime
-
-print("Setting up lab environment...")
-print("Roll No: 727823TUCY009")
-print("Timestamp:", datetime.datetime.now())
-
-print("Kali Linux and Metasploitable2 are assumed to be running.")
